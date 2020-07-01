56fb40082b13bff7806cb5dd68ac12b09f62b178
Vincent Siauw <jamesvincentsiauw@gmail.com>
2020-07-01 14:24:23 +0700

    refactor code

--- server/app.py
+++ server/app.py
@@ -14,9 +14,9 @@ def tampered_image_processing():
     try:
         requested_model = request.form['model']
 
-        if choose_model(requested_model) == "error":
+        if choose_model(requested_model) == 'error':
             val = {
-                'status': "error",
+                'status': 'error',
                 'message': 'Model Not Found!'
             }
             return jsonify(val), 400
@@ -27,13 +27,13 @@ def tampered_image_processing():
         # Process file
         if not request.files['img']:
             return jsonify({
-                'message': "Bad Parameter! Please upload file",
-                'status': "error"
+                'message': 'Bad Parameter! Please upload file',
+                'status': 'error'
             }), 400
         filepath = files_handler(request.files['img'])
-        if filepath == "error":
+        if filepath == 'error':
             val = {
-                'status': "error",
+                'status': 'error',
                 'message': 'Bad Parameter, Check File Extension!'
             }
             return jsonify(val), 400
@@ -46,17 +46,17 @@ def tampered_image_processing():
     except Exception as e:
         print(e)
         return {
-                   'status': "error",
+                   'status': 'error',
                    'message': e.args
                }, 500
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Create the application instance
     app = connexion.App(__name__, specification_dir='openapi/')
 
     # Add a flask route to expose information
-    app.add_url_rule("/api/predictor/health", "healthcheck", view_func=lambda: health.run())
+    app.add_url_rule('/api/predictor/health', 'healthcheck', view_func=lambda: health.run())
 
     # Read the swagger.yml file to configure the endpoints
     app.add_api('swagger.yaml')
