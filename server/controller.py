c82edb24e74a7bbd7da78fe2ec4fc0311cd3da8f
Vincent <jamesvincentsiauw@gmail.com>
2020-07-22 01:19:01 +0700

    refactor code

--- server/controller.py
+++ server/controller.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import shutil
 
-ALLOWED_EXTENSIONS = {'tif','jpg', 'jpeg', 'png'}
+ALLOWED_EXTENSIONS = {'tif', 'jpg', 'jpeg', 'png'}
 
 
 def allowed_file(filename):
@@ -16,27 +16,27 @@ def convert_to_ela_image(path, quality):
     filename = path
     resaved_filename = filename.split('.')[0] + '.resaved.jpg'
     ELA_filename = filename.split('.')[0] + '.ela.png'
-    
+
     im = Image.open(filename).convert('RGB')
     im.save(resaved_filename, 'JPEG', quality=quality)
     resaved_im = Image.open(resaved_filename)
-    
+
     ela_im = ImageChops.difference(im, resaved_im)
-    
+
     extrema = ela_im.getextrema()
     max_diff = max([ex[1] for ex in extrema])
     if max_diff == 0:
         max_diff = 1
     scale = 255.0 / max_diff
-    
+
     ela_im = ImageEnhance.Brightness(ela_im).enhance(scale)
-    
+
     return ela_im
 
 
 def load(filename):
     np_image = convert_to_ela_image(filename, 90)
-    np_image = np.array(np_image).astype('float32')/255
+    np_image = np.array(np_image).astype('float32') / 255
     np_image = transform.resize(np_image, (128, 128, 3))
     np_image = np.expand_dims(np_image, axis=0)
     return np_image
@@ -70,25 +70,26 @@ def files_handler(file):
     if not os.path.exists('datas/'):
         # make a directory if it doesn't exist
         os.makedirs('datas')
-    
+
     filepath = 'datas/'
     # save file to /datas/files/finalTasks
     file.save(os.path.join(filepath, file.filename))
 
-    return filepath+file.filename
+    return filepath + file.filename
 
 
 def process_prediction(model, img):
     try:
-        for i in zip(model.predict_proba(img, batch_size=50), model.predict_classes(img, batch_size=50), model.predict(img, batch_size=50)):
-                val = {
-                    'status': 'success',
-                    'message': 'Predicting Image Succeed',
-                    'results': {
-                        'prediction': get_class_name(i[1]),
-                        'probability': str(round(max(i[0])*100, 2))+'%'
-                    }
+        for i in zip(model.predict_proba(img, batch_size=50), model.predict_classes(img, batch_size=50),
+                     model.predict(img, batch_size=50)):
+            val = {
+                'status': 'success',
+                'message': 'Predicting Image Succeed',
+                'results': {
+                    'prediction': get_class_name(i[1]),
+                    'probability': str(round(max(i[0]) * 100, 2)) + '%'
                 }
+            }
         if os.path.exists('datas'):
             shutil.rmtree('datas')
         return val
