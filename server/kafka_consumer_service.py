9958b24b2e88ba0a793e14ac961a3cb489b708e5
Vincent <jamesvincentsiauw@gmail.com>
2020-07-22 13:38:44 +0700

    kafka consumer service

---
+++ server/kafka_consumer_service.py
@@ -0,0 +1,55 @@
+import base64
+import json
+import re
+import urllib.request
+import os
+from controller import *
+from kafka import KafkaConsumer
+from keras.models import load_model
+
+topic = 'tampered-image'
+bootstrap_servers = ['localhost:9092']
+consumer_timeout = 1000
+group_id = 'topic-group'
+
+consumer = KafkaConsumer(
+    bootstrap_servers=bootstrap_servers,
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+    )
+consumer.subscribe(topic)
+
+while True:
+    try:
+        for message in consumer:
+            print(
+                "> consuming message from %s partition=%d with offset=%d and key=%s" % (
+                    message.topic, message.partition, message.offset, message.key
+                )
+            )
+            try:
+                if message is None:
+                    print('No Message Received')
+                    continue
+                else:
+                    # Parse passed data to json format
+                    parsed_string = re.sub(r"[“|”|‛|’|‘|`|´|″|′|']", '"', str(message.value))
+                    json_data = json.loads(parsed_string)
+
+                    # check the directory to save the file
+                    if not os.path.exists('data/'):
+                        # make a directory if it doesn't exist
+                        os.makedirs('data')
+
+                    # Retrieve image from URL
+                    urllib.request.urlretrieve(json_data['img'], json_data['file'])
+
+                    # Process Prediction
+                    model = load_model(choose_model(json_data['model']))
+                    img = load(json_data['file'])
+                    print(process_prediction(model, img))
+            except Exception as e:
+                print(e)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        consumer.close()
